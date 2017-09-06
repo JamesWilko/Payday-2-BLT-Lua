01cfe318c9434ebea43e02eca33cf4c6d0d8e52a
TdlQ <github@z77.fr>
2017-09-06 20:02:05 +0200

    Fix Utils:GetCrosshairRay()

--- mods/base/req/utils/UtilsCore.lua
+++ mods/base/req/utils/UtilsCore.lua
@@ -1,4 +1,9 @@
 
+local mvec3_add = mvector3.add
+local mvec3_mul = mvector3.multiply
+local mvec3_set = mvector3.set
+local tmp_vec1 = Vector3()
+
 _G.Utils = _G.Utils or {}
 
 if false then
@@ -281,12 +286,21 @@ function Utils:GetCrosshairRay( from, to, slot_mask )
 
 	slot_mask = slot_mask or "bullet_impact_targets"
 
-	local player = managers.player:player_unit()
-	local from = from or player:camera():position()
-	local mvecTo = Vector3()
+	if not from then
+		local player = managers.player:player_unit()
+		if player then
+			from = player:movement():m_head_pos()
+		else
+			from = managers.viewport:get_current_camera_position()
+		end
+	end
 
-	mvector3.set( mvecTo, player:camera():forward() )
-	mvector3.add(to, from)
+	if not to then
+		to = tmp_vec1
+		mvec3_set( to, player:camera():forward() )
+		mvec3_mul( to, 20000 )
+		mvec3_add( to, from )
+	end
 
 	local colRay = World:raycast("ray", from, to, "slot_mask", managers.slot:get_mask(slot_mask))
 	return colRay
